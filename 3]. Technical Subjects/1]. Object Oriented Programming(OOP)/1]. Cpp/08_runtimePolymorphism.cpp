b7d7e9778bb62dd788d51758dec17d6866538088
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2026-04-25 03:31:52 +0600

    Runtime polymorphism with function overriding

---
+++ 3]. Technical Subjects/1]. Object Oriented Programming(OOP)/1]. Cpp/08_runtimePolymorphism.cpp
@@ -0,0 +1,40 @@
+// Function Overriding and Runtime Polymorphism in C++
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape {
+    public: 
+        virtual void draw() {
+            cout << "Drawing a generic shape" << endl;
+        }
+};
+
+class Circle: public Shape {
+    public:
+        void draw() override {
+            cout << "Drawing a circle" << endl;
+        }
+};
+
+class Rectangle: public Shape {
+    public:
+        void draw() override {
+            cout << "Drawing a rectangle" << endl;
+        }
+};
+
+
+
+int main() {
+    Shape *shape1 = new Circle();
+    Shape *shape2 = new Rectangle();
+
+    shape1->draw(); // Output: Drawing a circle
+    shape2->draw(); // Output: Drawing a rectangle
+
+    delete shape1;
+    delete shape2;
+
+    return 0;
+}
