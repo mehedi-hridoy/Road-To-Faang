81031e8ba6cbfa0e39f28d0bbe1f36cdb89ffa9c
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2026-05-13 18:29:42 +0600

    Valid anagram

---
+++ 1]. Data Structures/04]. Strings/06_validAnagram.cpp
@@ -0,0 +1,16 @@
+// 242 valid Anagram 
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string s, t;
+    cin >> s >> t; 
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+
+    if(s == t ) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+    
+}
